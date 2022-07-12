aa3018422b18d4b41e248cca3f962ef2bc1aa181
Yogi Delfiandra <yogi.delfiandra03@gmail.com>
2022-07-12 20:06:01 +0700

    add complete checkout page

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -114,7 +114,7 @@ export default function Checkout() {
         <div className='d-md-block d-flex flex-column w-100 pt-50'>
           <a
             className='btn btn-confirm-payment rounded-pill fw-medium text-white border-0 text-lg'
-            href='./complete-checkout.html'
+            href='./complete-checkout'
             role='button'
           >
             Confirm Payment
