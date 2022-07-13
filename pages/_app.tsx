e74a6187d70e036240e74f477c83a3026635b510
Yogi Delfiandra <yogi.delfiandra03@gmail.com>
2022-07-13 13:54:06 +0700

    add member pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import '../styles/404-not-found.css';
 import '../styles/checkout.css';
 import '../styles/complete-checkout.css';
 import '../styles/detail.css';
 import '../styles/homepage.css';
+import '../styles/overview.css';
+import '../styles/sidebar.css';
 import '../styles/sign-in.css';
 import '../styles/sign-up-photo.css';
 import '../styles/sign-up-success.css';
