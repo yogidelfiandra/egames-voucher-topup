c806124e63100326c163b21606ff16aa0b0de4b0
Yogi Delfiandra <yogi.delfiandra03@gmail.com>
2022-07-25 13:37:42 +0700

    add global style storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,26 @@
+import '../styles/404-not-found.css';
+import '../styles/checkout.css';
+import '../styles/complete-checkout.css';
+import '../styles/detail.css';
+import '../styles/edit-profile.css';
+import '../styles/homepage.css';
+import '../styles/navbar-log-in.css';
+import '../styles/overview.css';
+import '../styles/sidebar.css';
+import '../styles/sign-in.css';
+import '../styles/sign-up-photo.css';
+import '../styles/sign-up-success.css';
+import '../styles/sign-up.css';
+import '../styles/transactions-detail.css';
+import '../styles/transactions.css';
+import '../styles/utilities.css';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
